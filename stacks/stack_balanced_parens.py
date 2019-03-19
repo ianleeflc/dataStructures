d1f323a847a8ef60074194f43a78fc126a9781a6
Yiyan Li <yiyanli185@gmail.com>
2019-03-18 23:20:31 -0600

    modified stack paren check function to resolve the '(((' issue

--- stacks/stack_balanced_parens.py
+++ stacks/stack_balanced_parens.py
@@ -4,39 +4,39 @@
  example of unbalanced ones: ((), ))
 """
 
-# the first version, has some flaws
-from stack import Stack
-def is_match(p1,p2):
-    if p1=='(' and p2==')':
-        return True
-    elif p1=='[' and p2==']':
-        return True
-    elif p1=='{' and p2=='}':
-        return True
-    else: 
-        return False
-def is_paren_balanced(paren_string):
-    index=0
-    is_balanced=True
-    s=Stack()
-    while index<len(paren_string) and is_balanced==True:
-        if paren_string[index] in '([{':
-            s.push(paren_string[index])
-        else: 
-            if is_match(s.pop(), paren_string[index]):
-                is_balanced=True
-            else: 
-                is_balanced=False
-                return False
-        index+=1
-    if s.is_empty() and is_balanced==True:
-        return True
-
-print(is_paren_balanced('[]')) # works
-print(is_paren_balanced('([{}])')) # works
-print(is_paren_balanced('(([)')) # works
-print(is_paren_balanced('())')) # doesn't work
-print(is_paren_balanced('))')) # doesn't work
+## the first version, has some flaws
+#from stack import Stack
+#def is_match(p1,p2):
+#    if p1=='(' and p2==')':
+#        return True
+#    elif p1=='[' and p2==']':
+#        return True
+#    elif p1=='{' and p2=='}':
+#        return True
+#    else: 
+#        return False
+#def is_paren_balanced(paren_string):
+#    index=0
+#    is_balanced=True
+#    s=Stack()
+#    while index<len(paren_string) and is_balanced==True:
+#        if paren_string[index] in '([{':
+#            s.push(paren_string[index])
+#        else: 
+#            if is_match(s.pop(), paren_string[index]):
+#                is_balanced=True
+#            else: 
+#                is_balanced=False
+#                return False
+#        index+=1
+#    if s.is_empty() and is_balanced==True:
+#        return True
+#
+#print(is_paren_balanced('[]')) # works
+#print(is_paren_balanced('([{}])')) # works
+#print(is_paren_balanced('(([)')) # works
+#print(is_paren_balanced('())')) # doesn't work
+#print(is_paren_balanced('))')) # doesn't work
 
 # the imporoved version
 from stack import Stack
@@ -74,8 +74,57 @@ print(is_paren_balanced('([{}])')) # works
 print(is_paren_balanced('(([)')) # works
 print(is_paren_balanced('())')) # works
 print(is_paren_balanced('))')) # works
-
+print(is_paren_balanced('((')) # doesn't work 
+print(is_paren_balanced('')) # doesn't work 
+#
       
+## the imporoved version II
+#from stack import Stack
+#def is_match(p1,p2):
+#    if p1=='(' and p2==')':
+#        return True
+#    elif p1=='[' and p2==']':
+#        return True
+#    elif p1=='{' and p2=='}':
+#        return True
+#    else: 
+#        return False
+#def is_paren_balanced(paren_string):
+#    index=0
+#    is_balanced=True
+#    s=Stack()
+#    if paren_string == '': # handle the case of ''
+#        is_balanced=False
+#        return False
+#    elif paren_string[-1] in '([{': # cases like '((', '{{', '[[[[' will be handled by this
+#        is_balanced=False
+#        return False
+#    else: 
+#        while index<len(paren_string) and is_balanced==True:
+#            if paren_string[index] in '([{':
+#                s.push(paren_string[index])
+#            else: 
+#                if s.is_empty(): # case of '())', or '))', or the .pop operation is done
+#                    is_balanced=False
+#                    return False
+#                elif is_match(s.pop(), paren_string[index]):
+#                    is_balanced=True
+#                else: 
+#                    is_balanced==False
+#                    return False
+#            index+=1
+#    if s.is_empty() and is_balanced==True:
+#        return True
+#
+#print(is_paren_balanced('[]')) # works
+#print(is_paren_balanced('([{}])')) # works
+#print(is_paren_balanced('(([)')) # works
+#print(is_paren_balanced('())')) # works
+#print(is_paren_balanced('))')) # works
+#print(is_paren_balanced('((')) # works 
+#print(is_paren_balanced('')) # works 
+
+
 
 
 
